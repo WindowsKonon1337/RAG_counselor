562cd42addaca06c9a285588e3e0847be8aab48e
Maksim Kononykhin <kononelder@gmail.com>
2024-07-14 21:36:10 +0300

    add final prompt template

---
+++ src/prompt_templates/get_final_answer.py
@@ -0,0 +1,17 @@
+def configure_final_answer_prompt(label, text, query):
+    final_answer_frompt_template = \
+f'''
+!Задача!
+Определи наиболее релевантный пункт текста и дай ответ на вопрос
+
+!формат ответа !
+Запрос: <входящий запрос>
+Ответ: <число рублей> рублей
+Норма: <Норма>
+
+!Входные данные!
+Норма: {label}
+Текст закона: {text}
+Запрос: {query}
+'''
+    return final_answer_frompt_template
