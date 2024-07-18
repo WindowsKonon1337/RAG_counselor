2fdb7456e4544c056b0c4457e0ba24080a1e487f
Maksim Kononykhin <kononelder@gmail.com>
2024-07-19 00:11:57 +0300

    add test data for retrieval part & metric eval script

---
+++ tests/retrieval_acc_test.py
@@ -0,0 +1,28 @@
+import json
+import requests
+
+
+if __name__ == '__main__':
+    with open('./data/test_queries.json') as file:
+        test_objects = json.load(file)['test_objects']
+
+    good_ans = 0
+
+    for test_obj in test_objects:
+        response = requests.get(
+            f'http://localhost:1337/question?q_str={test_obj["query"]}'
+        )
+
+        respone_json = json.loads(
+            response.text
+        )
+
+        founded_law_norms = list(
+            x['law_norm'] for x in respone_json['metadatas'][0]
+        )
+
+        if test_obj['norm'] in founded_law_norms:
+            good_ans += 1
+
+    print(good_ans / len(test_objects))
+
